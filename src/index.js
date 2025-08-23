6ae379899c94decf95d8a583a54d423af521cf06
andesbit <adocarpel@gmail.com>
2025-08-23 07:58:36 -0500

    tercer jiro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 // server-express.js
 import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to Node.js');
+
+    //...ENTRADA....res.send('HELLO');
+        //...EXIST...
+    const ipath = join(__dirname, 'DB/document_a.json');
+    if (!existsSync(ipath)) {
+            throw new Error(`data base Key no encontrado: ${ipath}`);
+        }
+        //...WRITE...
+    const content =`
+        {
+            "ole": "SEWA",
+            "lisa": "I"
+        }`;
+    writeFileSync(ipath, content, {
+        encoding: "utf8", // utf8 es el valor por defecto
+        //mode: "0o666", // son los permisos, este valor puede variar dependiendo si te encuentras en Linux o Windows,
+        flag: "w", // w indica que el archivo se debe crear si no existe o su contenido se debe reemplazar si existiera
+    });
+        //...READ...
+    const st = JSON.parse(readFileSync(ipath, 'utf8'));
+    let pd = st.ole;
+    res.send(pd);
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });   
